03c7fb2681655336afa4e9264eafb5f86f9b3ca9
MaxZboro <71171333+maxzboro@users.noreply.github.com>
2023-07-13 11:47:27 +0200

    Add files via upload

---
+++ secret.js
@@ -0,0 +1,7 @@
+var pass = ""
+while (pass != "bash init catph #br master"){
+    pass = prompt("Error 666 Password Required")
+    if (pass != "bash init catph #br master"){
+        alert("INCORRECT!")
+    }
+}
