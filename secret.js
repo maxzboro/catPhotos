6fd75d2ef60dfa8b8fd40857614d9d0f3d02e41c
MaxZboro <71171333+maxzboro@users.noreply.github.com>
2024-10-29 12:59:25 +0200

    Delete secret.js - no secrets in it

--- secret.js
+++
@@ -1,7 +0,0 @@
-var pass = ""
-while (pass != "bash init catph #br master"){
-    pass = prompt("Error 666 Password Required")
-    if (pass != "bash init catph #br master"){
-        alert("INCORRECT!")
-    }
-}
